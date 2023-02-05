637da4a4ef49eeb21dbab4b6a049ba04bfd27add
jihooni0914 <dbwlgns8314@gmail.com>
2023-02-05 22:30:27 +0900

    implement main.sh

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,7 @@ FROM ubuntu:latest
 
 COPY . .
 
-# RUN apt-get update && \
-# apt-get install -y git
+RUN apt-get update && \
+    apt-get install -y git
 
-# ENTRYPOINT [ "sh", "-c", "./test.sh", "$github-repo-url", "$github-user-id", "$github-user-password" ]
-ENTRYPOINT [ "./test.sh" ]
+ENTRYPOINT [ "./main.sh" ]
---
+++ main.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+repo_url=$1
+repo_name=`echo \$repo_url | cut -d '/' -f5`
+repo_name=`echo \$repo_name | cut -d '.' -f1`
+echo "$repo_name"
+
+git clone $repo_url
+
+chmod +x make_package_dot_json.sh make_src_directory.sh make_appserver.sh make_config.sh make_routes.sh
+cd $repo_name
+ls
+../make_package_dot_json.sh $repo_name $repo_url
+../make_src_directory.sh
+../make_appserver.sh
+../make_config.sh
+../make_routes.sh
+
+npm install
+
+git add .
+git commit -m "init"
+git push
+# npm install
+# npm start
