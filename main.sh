3a05c0b742ae375cc2fcbad1b8f18fa7710b1158
jihooni0914 <dbwlgns8314@gmail.com>
2023-02-07 02:53:35 +0900

    change clone url

--- main.sh
+++ main.sh
@@ -5,8 +5,8 @@ repo_name=`echo \$repo_url | cut -d '/' -f5`
 repo_name=`echo \$repo_name | cut -d '.' -f1`
 echo "$repo_name"
 
-# git clone https://$5@github.com/$2/$repo_name
-git clone $repo_url
+git clone https://$2:$5@github.com/$2/$repo_name.git
+# git clone $repo_url
 
 chmod +x make_package_dot_json.sh make_src_directory.sh make_appserver.sh make_config.sh make_routes.sh
 cd $repo_name
@@ -29,4 +29,5 @@ git config --global user.password
 git add .
 git commit -m "init"
 # git push -u origin main
-git push https://$2:$5@github.com/$2/$repo_name.git
+git push
+# git push https://$2:$5@github.com/$2/$repo_name.git
