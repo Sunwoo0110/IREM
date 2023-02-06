99a8daab5664582a4afb0b4d72a237da30d892d6
sunwookim <sunkim09@g.skku.edu>
2023-02-06 21:28:07 +0900

    FEAT main

--- main.sh
+++ main.sh
@@ -3,10 +3,11 @@
 repo_url=$1
 repo_name=`echo \$repo_url | cut -d '/' -f5`
 repo_name=`echo \$repo_name | cut -d '.' -f1`
-echo "$repo_name"
+echo "repo name: $repo_name"
+echo "repo url: $repo_url"
 
 # git clone https://$5@github.com/$2/$repo_name
-git clone $repo_url
+# git clone $repo_url
 
 chmod +x make_package_dot_json.sh make_src_directory.sh make_appserver.sh make_config.sh make_routes.sh
 cd $repo_name
@@ -15,10 +16,10 @@ cd $repo_name
 ../make_appserver.sh
 ../make_config.sh
 ../make_routes.sh
-ls
 
 # npm install
 
+git clone git@github.com:$2/$repo_name.git
 git config --global user.name $2
 git config --global user.name 
 git config --global user.email $3
