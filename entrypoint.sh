c5bb135fe45a9e8ad9b938d581811c335a43c35b
jihooni0914 <81512592+jihooni0914@users.noreply.github.com>
2023-02-08 03:51:31 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,13 @@ repo_name=`echo \$repo_url | cut -d '/' -f5`
 repo_name=`echo \$repo_name | cut -d '.' -f1`
 echo "$repo_name"
 
+ls
 git clone https://$2:$4@github.com/$2/$repo_name.git
 
+ls
 chmod +x make_package_dot_json.sh make_src_directory.sh make_appserver.sh make_config.sh make_routes.sh make_env_sample.sh make_gitignore.sh
 cd $repo_name
+ls
 ./make_package_dot_json.sh $repo_name $repo_url
 ./make_src_directory.sh
 ./make_appserver.sh
