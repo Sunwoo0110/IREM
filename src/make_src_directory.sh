2352d3340c394a7f436c5a34749789b1d8003454
Sunwoo Kim <sunkim09@g.skku.edu>
2023-02-08 05:29:10 +0900

    Update make_src_directory.sh

--- src/make_src_directory.sh
+++ src/make_src_directory.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ## make src directory
-mkdir -p ./$1/src
+mkdir -p ../$1/src
 
 ## make config, routes directory
-mkdir -p ./$1/src/config
-mkdir -p ./$1/src/routes
+mkdir -p ../$1/src/config
+mkdir -p ../$1/src/routes
